df49140aaaa092d5d31eab5e11417ea61016c69f
Bayz.ng <108221204+Bayzng@users.noreply.github.com>
2025-09-05 12:12:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
